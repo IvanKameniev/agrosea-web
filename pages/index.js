e5ed8bd33bc948103eebac72b9ba21f48d5a1f94
Ivan Kameniev <80778099+kamiv1980@users.noreply.github.com>
2023-05-23 15:43:05 +0200

    test

--- pages/index.js
+++ pages/index.js
@@ -86,21 +86,22 @@ export default function Home() {
             </button>
           </div>
           }
+          <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
 
         </div>
       </main>
 
       <footer className={styles.footer}>
-        {/*<a*/}
-        {/*  href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"*/}
-        {/*  target="_blank"*/}
-        {/*  rel="noopener noreferrer"*/}
-        {/*>*/}
-        {/*  Powered by{' '}*/}
-        {/*  <span className={styles.logo}>*/}
-        {/*    <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />*/}
-        {/*  </span>*/}
-        {/*</a>*/}
+        <a
+          href="https://agrosea.com.ua/en"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Powered by{' '}
+          <span className={styles.logo}>
+            AgroSea
+          </span>
+        </a>
       </footer>
     </div>
   )
